7a1e3b04a8156d786457bad8f9f4f03e57164b95
minato <nnkm8329@gmail.com>
2026-04-26 13:37:06 +0900

    Initial Commit

---
+++ beer_cheers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  beer_cheers
+//
+//  Created by 石田湊 on 2026/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ beer_cheers/beer_cheersApp.swift
@@ -0,0 +1,17 @@
+//
+//  beer_cheersApp.swift
+//  beer_cheers
+//
+//  Created by 石田湊 on 2026/04/26.
+//
+
+import SwiftUI
+
+@main
+struct beer_cheersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
